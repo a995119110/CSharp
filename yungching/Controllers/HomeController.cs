eb2ca4871887f7a9b937533e975b53a0decc9623
a995119110 <a995119110@gmail.com>
2021-10-13 19:52:09 +0800

    Create and Delete

--- yungching/Controllers/HomeController.cs
+++ yungching/Controllers/HomeController.cs
@@ -51,6 +51,56 @@ namespace yungching.Controllers
             return View(categorie);
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult InsertValue(Categories modelCategories)
+        {
+            if (ModelState.IsValid)
+            {
+                northwind.Categories.Add(modelCategories);
+                northwind.SaveChanges(); //建議使用SaveChangesAsync 動態分配時間 刪除同理
+
+                //northwind.Categories.Add(modelCategories);
+                //await northwind.SaveChangesAsync();
+
+                return RedirectToAction("List");
+            }
+            return View(modelCategories);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categorie = northwind.Categories.Find(id);
+            if (categorie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categorie);
+
+        }
+
+        [HttpPost]
+        [ActionName("Detele")]
+        public ActionResult DeteleValue(int? id)
+        {
+            if (id != null)
+            {
+                Categories categories = northwind.Categories.Find(id);
+                northwind.Categories.Remove(categories);
+                northwind.SaveChanges();
+                return RedirectToAction("List");
+            }
+            else
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
     }
 }
